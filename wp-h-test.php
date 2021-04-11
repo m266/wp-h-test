f08ff57f4701142ab535761872f94f2edcec9ae9
m266 <herbrand@mailbox.org>
2021-04-11 13:02:24 +0200

    Add files via upload

---
+++ wp-h-test.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name:        WP H-Test
+Plugin URI:         https://github.com/m266/wp-h-test
+Description:        Test-Plugin fuer Tutorial
+Author:             Hans M. Herbrand
+Author URI:         https://www.web266.de
+Version:            1.0
+Date:               2019-05-04
+Text Domain:        wp-h-test
+License:            GNU General Public License v2 or later
+License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI:  https://github.com/m266/wp-h-test
+ */
+
+// Plugin-Code
